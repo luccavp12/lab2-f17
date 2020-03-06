40ac81cb9a73f5da9543b02180af6aeeece609d3
Lucca Psaila <lucca12vp@gmail.com>
2020-03-06 13:51:16 -0800

    Altered memlayout

--- memlayout.h
+++ memlayout.h
@@ -6,9 +6,11 @@
 
 // Key addresses for address space layout (see kmap in vm.c for layout)
 #define KERNBASE 0x80000000         // First kernel virtual address
-#define STACKBASE (KERNBASE - 0x1)  // First Stack address, 1 address directly below 
+// #define STACKBASE (KERNBASE - 0x1)  // First Stack address, 1 address directly below 
                                     // the kernel memory
 #define KERNLINK (KERNBASE+EXTMEM)  // Address where kernel is linked
+
+#define STACKBASE (KERNBASE - 0x1)  // First Stack address, 1 address directly below 
 
 #define V2P(a) (((uint) (a)) - KERNBASE)
 #define P2V(a) (((void *) (a)) + KERNBASE)
