9ff51de5f6024bef28bd94c0164341ee8c47bf9a
Lucca Psaila <lucca12vp@gmail.com>
2020-03-06 23:16:11 -0800

    Update exec.c

--- exec.c
+++ exec.c
@@ -66,12 +66,8 @@ exec(char *path, char **argv)
 
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
-  // sz = PGROUNDUP(sz); //round sz up to the next page boundary since stack must start in a new page
-  // if((sz = allocuvm(pgdir, STACKBASE, STACKBASE - PGSIZE)) == 0) 
-    //Need to change the allocuvm() parameters in order to move the stack to the end
-    // goto bad;
-  // clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
-  sp = STACKBASE; // Change stack pointer to address of the top word in the stack page
+  sp = STACKBASE; 
+  // Change stack pointer to address of the top word in the stack page
   // Since STACKBASE is 1 space below the base of the Kernel space, we want to point
   // to the position all the way to the top of the stack
 
